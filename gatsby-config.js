e239ccf814fe9c3521b2a379dfb2b21e40b29497
tatsuoNakano <aaaoauo1118@gmeil.com>
2024-02-06 16:58:15 +0900

    gatsbyチュートリアル分の機能は追加したのでいったん区切り　devからfeature切って個別に機能を特化して開発していきます

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,17 +6,37 @@ module.exports = {
     title: `gatsby-starter-tricycle`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+      "gatsby-plugin-image",
+      "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `post`,
+        path: `${__dirname}/post`,
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `fix`,
+        path: `${__dirname}/fix`,
+      }
+    },
+    "gatsby-transformer-sharp", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  }
+  ]
 };
---
+++ src/pages/post/{mdx.frontmatter__slug}.js
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../../component/layout/layout'
+import Seo from '../../component/function/seo'
+
+const Post = ({ data, children }) => {
+    return (
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <p>{data.mdx.frontmatter.date}</p>
+            {children}
+        </Layout>
+    )
+}
+
+export const query = graphql`
+    query($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+            }
+        }
+    }
+
+
+`
+
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
+
+export default Post
