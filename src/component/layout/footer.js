b81e124006bd05ce966b5b824e3602bfd7e3043a
tatsuoNakano <aaaoauo1118@gmeil.com>
2024-02-13 14:52:33 +0900

    footerを作った

--- src/component/layout/footer.js
+++ src/component/layout/footer.js
@@ -17,19 +17,18 @@ const LINKS = [
 
 const currentYear = new Date().getFullYear();
 const Footer = () => (
-    <footer className="relative w-full">
-        <div className="mx-auto w-full max-w-7xl px-8">
+    <footer className="relative  bg-gray-300 m-2.5">
+        <div className="mx-auto mt-2.5  w-full max-w-5xl px-2.5 bg-gray-300">
             <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
-                <Typography variant="h5" className="mb-6">
-                    Material Tailwind
+                <Typography variant="h5" className="mb-2.5 mt-2.5" >
+                    Gatsby-starter-tricycle
                 </Typography>
-                <div className="grid grid-cols-3 justify-between gap-4">
+                <div className="grid grid-cols-3 justify-between gap-2.5">
                     {LINKS.map(({title, items}) => (
                         <ul key={title}>
                             <Typography
-                                variant="small"
-                                color="blue-gray"
-                                className="mb-3 font-medium opacity-40"
+                                color="black"
+                                className="mb-2.5 font-medium"
                             >
                                 {title}
                             </Typography>
@@ -38,8 +37,8 @@ const Footer = () => (
                                     <Typography
                                         as="a"
                                         href="#"
-                                        color="gray"
-                                        className="py-1.5 font-normal transition-colors hover:text-blue-gray-900"
+                                        color="black"
+                                        className="py-2.5 font-normal transition-colors hover:text-blue-gray-900"
                                     >
                                         {link}
                                     </Typography>
@@ -50,15 +49,15 @@ const Footer = () => (
                 </div>
             </div>
             <div
-                className="mt-12 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
+                className="mt-2.5 flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
                 <Typography
                     variant="small"
-                    className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
+                    className="mb-2.5 text-center font-normal text-blue-gray-900 md:mb-0"
                 >
-                    &copy; {currentYear} <a href="https://material-tailwind.com/">Material Tailwind</a>. All
+                    &copy; {currentYear} <a href="https://github.com/tatsuoNakano/gatsby-starter-tricycle">Gatsby-starter-tricycle</a>. All
                     Rights Reserved.
                 </Typography>
-                <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
+                <div className="flex gap-2.5 text-blue-gray-900 sm:justify-center">
                     <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100">
                         <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                             <path
