9d112966126ed372c1c9fc007445a98496fabc07
tatsuoNakano <aaaoauo1118@gmeil.com>
2024-02-14 18:21:34 +0900

    スクリーンショット

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import Layout from '../component/layout/layout'
 import { Seo } from "../component/function/seo"
+import {StaticImage} from "gatsby-plugin-image";
 
 // Step 2: Define your component
 const IndexPage = () => {
@@ -10,16 +11,12 @@ const IndexPage = () => {
           <Layout pageTitle="Home Page">
               <h1 className="mb-2 padding-b text-xl font-semibold text-gray-900 bg-gradient-to-br from-gray-200 to-gray-300 p-4 shadow-md border-l-4 border-blue-500">Thank
                   you for installing Gatsby-starter-tricycle!</h1>
-              <ol>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-              </ol>
-              <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-              </ul>
+              <StaticImage className="m-2.5" src="../images/parts/screenshot/xl.PNG" alt="サイト"/>
+              <StaticImage className="m-2.5"  src="../images/parts/screenshot/xm.PNG" alt="サイト"/>
+              <StaticImage className="m-2.5"  src="../images/parts/screenshot/xs1.PNG" alt="サイト"/>
+              <StaticImage className="m-2.5"  src="../images/parts/screenshot/xs2.PNG" alt="サイト"/>
+              <StaticImage className="m-2.5"  src="../images/parts/screenshot/xs3.PNG" alt="サイト"/>
+              <StaticImage className="m-2.5"  src="../images/parts/screenshot/xs-tag.PNG" alt="サイト"/>
           </Layout>
       </main>
   )
